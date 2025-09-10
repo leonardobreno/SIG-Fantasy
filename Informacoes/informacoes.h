35a6bf9d72e8c1954d029e23d4638c4886813f4d
Pedro1349 <pedrolucaspc87@gmail.com>
2025-09-10 15:51:55 -0300

    Feature: informacoes_modularizada

---
+++ Informacoes/informacoes.h
@@ -0,0 +1,6 @@
+
+
+char menu_informacoes(void);
+void menu_sobre_projeto(void);
+void menu_equipe(void);
+void modulo_informacoes(void);
--- projeto_sig_fantasy.c
+++ projeto_sig_fantasy.c
@@ -5,15 +5,11 @@
 #include <string.h>
 
 #include "Utilidades/utilidades.h"
+#include "Informacoes/informacoes.h"
 
 // Assinaturas das funções
 char menu_principal(void);
 char saida_programa(void);
-
-char menu_informacoes(void);
-void menu_sobre_projeto(void);
-void menu_equipe(void);
-void modulo_informacoes(void);
 
 char menu_cliente(void);
 void menu_lista_cliente(char[], char[], char[], char[]);
@@ -144,90 +140,6 @@ char saida_programa(void) {
     return opcao;
 }
 
-char menu_informacoes(void) {
-    char op;
-    system("clear||cls");
-    printf("╔═════════════════════════════════════════════════════════════════════════════════╗\n");
-    printf("║                                       SIG-Fantasy:                              ║\n");
-    printf("║                                                                                 ║\n");
-    printf("║                                    Modulo Informações                           ║\n");
-    printf("╠═════════════════════════════════════════════════════════════════════════════════╣\n");
-    printf("║                                 -> 1 • Sobre o projeto                          ║\n");
-    printf("║                                 -> 2 • Equipe desenvolvedora                    ║\n");
-    printf("║                                 -> 3 •     Voltar                               ║\n");
-    printf("╚═════════════════════════════════════════════════════════════════════════════════╝\n");
-
-    printf("Escolha uma opção: ");
-    op = getchar();
-    limpar_buffer();
-    return op;
-}
-
-void menu_sobre_projeto(void) {
-    system("clear||cls");
-    printf("╔═════════════════════════════════════════════════════════════════════════════════╗\n");
-    printf("║                                    SIG-Fantasy:                                 ║\n");
-    printf("║                                                                                 ║\n");
-    printf("║                                  Sobre o projeto                                ║\n");
-    printf("╠═════════════════════════════════════════════════════════════════════════════════╣\n");
-    printf("║                   Programa desenvolvido com o fim de servir como                ║\n");
-    printf("║                   projeto para disciplina de Programacao(DCT1106)               ║\n");
-    printf("║                   Este programa serve como um sistema de gerencia               ║\n");
-    printf("║                   para uma loja de fantasias/roupas. O sistema                  ║\n");
-    printf("║                   permite que os clientes comprem ou aluguem                    ║\n");
-    printf("║                   os produtos da loja, alem de guardar os pedidos               ║\n");
-    printf("╚═════════════════════════════════════════════════════════════════════════════════╝\n");
-    printf("\nPressione Enter para voltar...\n");
-    limpar_buffer();
-}
-
-void menu_equipe(void) {
-    system("clear||cls");
-    printf("╔═════════════════════════════════════════════════════════════════════════════════╗\n");
-    printf("║                                      SIG-Fantasy:                               ║\n");
-    printf("║                                                                                 ║\n");
-    printf("║                                   Equipe do projeto                             ║\n");
-    printf("╠═════════════════════════════════════════════════════════════════════════════════╣\n");
-    printf("║                        Este projeto foi desenvolvido por:                       ║\n");
-    printf("║                                 Leonardo Breno da Silva Santos,                 ║\n");
-    printf("║                                 Pedro Lucas da Silva Araújo                     ║\n");
-    printf("║                                                                                 ║\n");
-    printf("║                        E-mails: leonardodbreno@gmail.com,                       ║\n");
-    printf("║                                 pedro.araujo.716@ufrn.edu.br                    ║\n");
-    printf("║                                                                                 ║\n");
-    printf("║                        GitHub: https://github.com/leonardobreno                 ║\n");
-    printf("║                                https://github.com/Pedro1349                     ║\n");
-    printf("╚═════════════════════════════════════════════════════════════════════════════════╝\n");
-    printf("\nPressione Enter para voltar...\n");
-    limpar_buffer();
-}
-
-void modulo_informacoes(void) {
-    char op;
-
-    do{
-        op = menu_informacoes();
-        switch (op)
-        {
-        case '1':
-            menu_sobre_projeto();
-            break;
-        case '2':
-            menu_equipe();
-            break;
-        
-        case '3':
-            printf("Voltando ao menu principal...\n");
-            sleep(1);
-            break;
-        default:
-            printf("Opção inválida!\n");
-            sleep(1);
-        }
-
-    } while (op != '3');
-    
-}
 
 char menu_cliente(void) {
     char op;
