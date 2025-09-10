94d282363b93f0330c3e10976d27f3c75d723d9d
Pedro1349 <pedrolucaspc87@gmail.com>
2025-09-10 16:06:14 -0300

    Feature: menu_principal e saida_programa modularizados

--- Utilidades/utilidades.c
+++ Utilidades/utilidades.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <unistd.h>
+#include <string.h>
 
 void limpar_buffer(void) {
     int c;
@@ -6,4 +10,68 @@ void limpar_buffer(void) {
     while ((c != '\n' && c != EOF)) {
         c = getchar();
     };
+}
+
+char menu_principal(void) {
+    char op;
+    system("clear||cls");
+    printf("╔═════════════════════════════════════════════════════════════════════════════════╗\n");
+    printf("║                                                                                 ║\n");
+    printf("║   ╔═══╗  ╔╗  ╔═══╗         ╔═══╗ ╔═══╗   ╠╗    ║ ╔══════╗ ╔═══╗  ╔═══╗ ║   ║    ║\n");
+    printf("║   ║      ║║  ║             ║     ║ ╔╗ ║  ║╚╗   ║ ╚══╗╔══╝ ║ ╔╗ ║ ║     ╚╗ ╔╝    ║\n");
+    printf("║   ╚═══╗  ║║  ║ ╔═╗ ╔═════╗ ╠══╗  ║ ╚╝ ║  ║ ╚═╗ ║    ║║    ║ ╚╝ ║ ╚═══╗  ╚╦╝     ║\n");
+    printf("║       ║  ║║  ║   ║ ╚═════╝ ║     ║ ╔╗ ║  ║   ╚╗║    ║║    ║ ╔╗ ║     ║   ║      ║\n");
+    printf("║   ╚═══╝  ╚╝  ╚═══╝         ║     ╠═╝╚═╣  ║    ╚╣    ║║    ╠═╝╚═╣ ╚═══╝   ║      ║\n");
+    printf("╚═════════════════════════════════════════════════════════════════════════════════╝\n");
+    setlocale(LC_ALL, "Portuguese");
+
+    printf("╔═════════════════════════════════════════════════════════════════════════════════╗\n");
+    printf("║                                     SIG-Fantasy:                                ║\n");
+    printf("║                                                                                 ║\n");
+    printf("║                             Loja de fantasias e roupas                          ║\n");
+    printf("╠═════════════════════════════════════════════════════════════════════════════════╣\n");
+    printf("║                              -> 1 • Modulo Clientes                             ║\n");
+    printf("║                              -> 2 • Modulo Fantasias                            ║\n");
+    printf("║                              -> 3 • Modulo Funcionarios                         ║\n");
+    printf("║                              -> 4 • Modulo Pedidos                              ║\n");
+    printf("║                              -> 5 • Modulo Informações                          ║\n");
+    printf("║                              -> 6 • Modulo Relatórios                           ║\n");
+    printf("║                              -> 0 •      Sair                                   ║\n");
+    printf("╚═════════════════════════════════════════════════════════════════════════════════╝\n");
+    printf("Escolha uma opção: ");
+    scanf(" %c", &op);
+    limpar_buffer();
+    printf("Carregando...\n");
+    sleep(1);
+    return op;
+}
+
+char saida_programa(void) {
+    int confirmacao = 0;
+    char opcao;
+    while (confirmacao == 0){
+        printf("Tem certeza que deseja sair do programa? (s/n)\n");
+        scanf(" %c", &opcao);
+        limpar_buffer();
+        if (opcao == 's' || opcao == 'S')
+        {
+            printf("Fechando o programa...\n");
+            opcao = '0';
+            confirmacao = 1;
+            sleep(1);
+
+        } else if (opcao == 'n' || opcao == 'N')
+        {
+            printf("Cancelando a saida...");
+            confirmacao = 1;
+            sleep(1);
+        } else
+        {
+            printf("opcao invalida\n");
+            opcao = 'n';
+            sleep(1);
+        }
+    
+    }
+    return opcao;
 }
--- Utilidades/utilidades.h
+++ Utilidades/utilidades.h
@@ -1,2 +1,4 @@
-//Assinatura da função "limpar_buffer"
+//Assinatura das funções: "limpar_buffer"; "menu_principal"; "saida_programa"
 void limpar_buffer(void);
+char menu_principal(void);
+char saida_programa(void);
