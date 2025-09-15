e5c9b26f6d67b6786771fe13821d69bb2b6b0128
Pedro1349 <pedrolucaspc87@gmail.com>
2025-09-14 22:38:49 -0300

    fix: recriei o arquivo do main

--- Clientes/clientes.c
+++ Clientes/clientes.c
@@ -6,49 +6,6 @@
 
 #include "../Utilidades/utilidades.h"
 
-void modulo_cliente(void);
-void modulo_fantasia(void);
-void modulo_funcionario(void);
-void modulo_pedido(void);
-void modulo_informacoes(void);
-
-int main(void) {
-    char opc;
-    do {
-        opc = menu_principal();
-        switch(opc) {
-            case '1':
-                modulo_cliente();
-                break;
-            case '2':
-                modulo_fantasia();
-                break;
-            case '3':
-                modulo_funcionario();
-                break;
-            case '4':
-                modulo_pedido();
-                break;                
-            case '5':
-                modulo_informacoes();
-                break;
-            case '6':
-                printf("\nModulo em processo de criacao.");
-                printf("\nVoltando ao menu...");
-                sleep(2);
-                break;
-            case '0':
-                opc = saida_programa();
-                break;
-            default:
-                printf("Opção inválida. Tente novamente.\n");
-                sleep(1);
-        }
-    } while(opc != '0');
-
-    return 0;
-}
-
 char menu_cliente(void) {
     char op;
     system("clear||cls");
---
+++ projeto.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "Utilidades/utilidades.h"
+#include "Informacoes/informacoes.h"
+#include "Pedidos/pedidos.h"
+#include "Funcionarios/funcionarios.h"
+#include "Fantasias/fantasias.h"
+#include "Clientes/clientes.h"
+
+int main(void) {
+    char opc;
+    do {
+        opc = menu_principal();
+        switch(opc) {
+            case '1':
+                modulo_cliente();
+                break;
+            case '2':
+                modulo_fantasia();
+                break;
+            case '3':
+                modulo_funcionario();
+                break;
+            case '4':
+                modulo_pedido();
+                break;                
+            case '5':
+                modulo_informacoes();
+                break;
+            case '6':
+                printf("\nModulo em processo de criacao.");
+                printf("\nVoltando ao menu...");
+                sleep(2);
+                break;
+            case '0':
+                opc = saida_programa();
+                break;
+            default:
+                printf("Opção inválida. Tente novamente.\n");
+                sleep(1);
+        }
+    } while(opc != '0');
+
+    return 0;
+}
