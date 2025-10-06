f5de807224bf47612409c4d5587f21a53042c92f
Thiagogomes1805 <thiago.oliveira.712@ufrn.edu.br>
2025-10-06 15:51:40 -0300

    Delete SIG-Fantasy-fantasy-teste directory

--- SIG-Fantasy-fantasy-teste/Pedidos/pedidos.h
+++
@@ -1,9 +0,0 @@
-
-// Assinaturas da funcoes relacionadas ao modulo de pedidos
-char menu_pedido(void);
-void menu_lista_pedido(int*, int*, float*, char[]);
-void menu_cadastro_pedido(int*, int*, float*, char[]);
-void menu_alterar_pedido(int*, int*, float*, char[]);
-void menu_deletar_pedido(int*, int*, float*, char[]);
-void modulo_pedido(void);
-void cad_pedido(char id_fantasia[], char id_cliente[], char preco[], char data_pedido[]);
--- SIG-Fantasy-fantasy-teste/Utilidades/utilidades.h
+++
@@ -1,4 +0,0 @@
-//Assinatura das funções: "limpar_buffer"; "menu_principal"; "saida_programa"
-void limpar_buffer(void);
-char menu_principal(void);
-char saida_programa(void);
