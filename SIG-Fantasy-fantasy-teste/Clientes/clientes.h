336c660e24ab4b2e9095869805285275764f583c
Thiagogomes1805 <thiago.oliveira.712@ufrn.edu.br>
2025-10-06 15:52:47 -0300

    Delete SIG-Fantasy-fantasy-teste directory

--- SIG-Fantasy-fantasy-teste/Clientes/clientes.h
+++
@@ -1,8 +0,0 @@
-//Assinatura das funções do modulo clientes
-char menu_cliente(void);
-int menu_pesquisar_cliente(char[]);
-void menu_cadastro_cliente(char[], char[], char[], char[]);
-void menu_alterar_cliente(char[], char[], char[], char[]);
-void menu_deletar_cliente(char[], char[], char[], char[]);
-void modulo_cliente(void);
-void cad_cliente(char nome[], char cpf[], char celular[], char email[]);
--- SIG-Fantasy-fantasy-teste/Fantasias/fantasias.h
+++
@@ -1,8 +0,0 @@
-//Assinatura das funções do modulo fantasias
-char menu_fantasia(void);
-int menu_pesquisar_fantasia(char fantasia_pesquisar[]);
-void menu_cadastro_fantasia(char nome_fantasia[], char tamanho[], char cor[]);
-void menu_alterar_fantasia(char fantasia_pesquisar[]);
-void menu_deletar_fantasia(char fantasia_pesquisar[]);
-void modulo_fantasia(void);
-void cad_fantasia(char nome_fantasia[], char tamanho[], char cor[]);
