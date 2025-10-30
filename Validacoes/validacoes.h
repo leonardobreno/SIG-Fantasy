0e25bc51ef421f52002a98339bb4aaf800cd1c05
thiagogomes1805 <thiago.oliveira.712@ufrn.edu.br>
2025-10-29 23:46:45 -0300

    Feat validacoes.h

--- Validacoes/validacoes.h
+++ Validacoes/validacoes.h
@@ -1,23 +1,15 @@
-#ifndef VALIDACAO_H
-#define VALIDACAO_H
+#ifndef VALIDACOES_H
+#define VALIDACOES_H
 
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#include <stdbool.h>
-#include <time.h>
-#include <locale.h>
-
-
+int validar_nome(char *nome);
 int validar_cpf(char *cpf);
+int validar_celular(char *celular);
+int validar_email(char *email);
+int validar_salario_string(char *salario);
 int validar_id_fantasia(const char *obs);
+int validar_tamanho_fantasia(char *tamanho);
+int validar_cor_fantasia(char *cor);
 int validar_preco(float preco);
 int valida_data(char *data);
-int ano_bissexto(int ano);
-int valida_dia(int dia, int mes, int ano);
-int ano_atual(void);
-int valida_ano(int ano);
 
-#endif // VALIDACAO_H
+#endif
