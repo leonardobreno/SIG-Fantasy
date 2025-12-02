7c9113c11d1b9558d281e6c3bd5271523457e21b
thiagogomes1805 <thiago.oliveira.712@ufrn.edu.br>
2025-12-01 23:20:05 -0300

    fix relatorios.h

--- Relatorios/relatorios.h
+++ Relatorios/relatorios.h
@@ -3,7 +3,7 @@
 
 
 void modulo_relatorios(void);
-char menu_relatorios(void);
+int menu_relatorios(void);
 void relatorio_fantasias_ativas(void);
 void relatorio_funcionarios_ativos(void);
 void relatorio_pedidos_ativos(void);
